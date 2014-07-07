88945b6902149639ab6c956d23ce88f128f4b69d
Michael Wiss <michael.wiss@gmail.com>
2014-07-07 13:20:46 -0400

    bcrypt and rspec gems

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
+
+#Use bcrypt for authentication
+gem 'bcrypt-ruby', '~> 3.1.2'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
@@ -31,6 +35,10 @@ gem 'jbuilder', '~> 1.2'
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0'
 end
 
 group :production do
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707160759) do
+ActiveRecord::Schema.define(version: 20140707170106) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email"
 
 end
