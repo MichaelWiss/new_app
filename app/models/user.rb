940d982a91b4e06c32980a117c0b510146ea2365
Michael Wiss <michael.wiss@gmail.com>
2014-07-07 16:22:53 -0400

    changed user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-	validates :email, presence: true
+	validates :email, presence: true,
+                     uniqueness: true
+
+before_save :downcase_email
+
+def downcase_email
+	self.email = email.downcase_email
+end
 end
