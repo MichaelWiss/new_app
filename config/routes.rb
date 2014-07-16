b052751ddd637bae860430b800e92054f4ded3ed
Michael Wiss <michael.wiss@gmail.com>
2014-07-16 13:46:52 -0400

    fixed root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ NewApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root 'static_pages#geolocation'
  
 
   # Example of regular route:
